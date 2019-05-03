8641e3af9e15334d48c371c7ac47667facbf47dc
Christopher Luna <chris@illusionlayervfx.com>
2019-05-03 05:51:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
     artist = ary[0]
     name = ary[1].split('.mp3')
     song = self.new
-    song.name.to_s = name
+    song.name = name.to_s
 
     song.artist_name = artist
     song
