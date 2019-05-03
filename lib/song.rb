b360a3efe6a14d52beae426f8be3f28897f36142
Christopher Luna <chris@illusionlayervfx.com>
2019-05-03 01:24:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,10 +4,10 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-  #def initialize(name)
-    #@name = name
+  def initialize(name)
+    @name = name
     #@@all << self
-  #end
+  end
 
   def self.all
     @@all
